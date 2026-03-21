a8266c7316db9616afdfb4a034380334ac7247e4
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-03-21 19:49:51 +0600

    добавили bcrypt

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
     username: {
@@ -13,6 +16,22 @@ const UserSchema = new Schema({
     token: {
         type: String,
         required: true,
+    },
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+
+    this.password = hash;
+});
+
+UserSchema.set('toJSON', {
+    transform: (_doc, ret) => {
+        const { password, __v, ...rest } = ret;
+        return rest;
     },
 });
 
